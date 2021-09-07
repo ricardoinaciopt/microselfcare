3d1d5a40a2434ff783a465b3a857fdcab208fd61
Ricardo Inácio <ricardo.inacio.2002@gmail.com>
2021-09-07 16:12:20 +0100

    app is now distributable for windows > maybe osx in the future?

--- main.js
+++ main.js
@@ -81,3 +81,5 @@ app.whenReady().then(() => {
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
+
+if (require('electron-squirrel-startup')) return app.quit();
