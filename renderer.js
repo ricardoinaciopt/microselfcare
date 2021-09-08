18c1d0ee3715ce73d56a1d94a952dbf7588015be
Ricardo Inácio <ricardo.inacio.2002@gmail.com>
2021-09-08 23:31:51 +0100

    main screen stats made

--- renderer.js
+++ renderer.js
@@ -2,7 +2,14 @@ const ipc = require('electron').ipcRenderer;
 const Store = require('electron-store')
 const AOS = require('aos')
 
+
 AOS.init();
+const store = new Store();
+
+
+new Notification("Take a Break", {title: "Remember to rest" , body: "Here is your reminder to rest a bit to make sure you are not overworking.", icon: "icon.ico", timeoutType: "never" })
+.onclick = () => console.log("CLICK_MESSAGE")
+
 
 if(document.querySelector('#maxi')){
   document.querySelector('#maxi').addEventListener('click', () => {
@@ -18,7 +25,76 @@ document.querySelector('#x').addEventListener('click', () => {
   ipc.send('x');
 })
 
-const store = new Store();
+if(document.querySelector('#timeofday')){
+  function timeOfDay(){
+    var d = new Date();
+    var n = d.getHours();
+    console.log(n);
+    if (n >= 21 || n < 6){
+      document.getElementById("timeofday").textContent = "Hope you're having a nice night";
+    }else if (n >= 18 || n < 21) {
+      document.getElementById("timeofday").textContent = "Good evening to you";
+    }else if (n >= 6 || n < 12) {
+      document.getElementById("timeofday").textContent = "Have a wonderful morning";
+    }else if (n >= 12 || n < 18) {
+      document.getElementById("timeofday").textContent = "Good afternoon";
+    }
+    setTimeout(timeOfDay, 3600000);
+  }
+  timeOfDay();
+  document.getElementById("user").textContent = store.get('user');
+
+  //stats
+  switch (store.get('goal')) {
+    case 'sleep':
+    document.getElementById("user-goal").textContent = "to sleep better.";
+    break;
+    case 'addiction':
+    document.getElementById("user-goal").textContent = "to quit an addiction.";
+    break;
+    case 'finnish':
+    document.getElementById("user-goal").textContent = "to finnish a project.";
+    break;
+  }
+
+  switch (store.get('struggle')) {
+    case 'time':
+    document.getElementById("user-struggle").textContent = "trouble at tracking time";
+    break;
+    case 'done':
+    document.getElementById("user-struggle").textContent = "not getting things done.";
+    break;
+    case 'care':
+    document.getElementById("user-struggle").textContent = "not taking good care of myself.";
+    break;
+  }
+
+  switch (store.get('mood')) {
+    case 'happy':
+    document.getElementById("user-mood").textContent = "happy.";
+    break;
+    case 'sad':
+    document.getElementById("user-mood").textContent = "sad.";
+    break;
+    case 'mad':
+    document.getElementById("user-mood").textContent = "mad.";
+    break;
+    case 'angry':
+    document.getElementById("user-mood").textContent = "anxious.";
+    break;
+  }
+
+  document.querySelector('#me').addEventListener('click', () => {
+    document.getElementById("mystats").style.visibility='visible';
+    document.getElementById("overlay").style.visibility='visible';
+  })
+
+  document.querySelector('#statx').addEventListener('click', () => {
+    document.getElementById("mystats").style.visibility='hidden';
+    document.getElementById("overlay").style.visibility='hidden';
+  })
+
+}
 
 if(document.querySelector('#button')){
   document.querySelector('#button').addEventListener('click', () => {
